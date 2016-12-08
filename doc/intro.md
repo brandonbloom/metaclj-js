3d2b2f33be0d63fb73ac8b0b2ea67e29446e9c00
Brandon Bloom <brandon@brandonbloom.name>
2016-12-07 16:41:05 -0800

    lein new bbloom.js

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to bbloom.js
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/bbloom/js_test.clj
@@ -0,0 +1,7 @@
+(ns bbloom.js-test
+  (:require [clojure.test :refer :all]
+            [bbloom.js :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
